035d673e4166139efeb661817d443914de708ee7
NicholasColotouros <agentnnc@gmail.com>
2013-12-27 00:00:52 -0500

    plan some code

---
+++ CustomCountdownTimer/src/cct/gui/CustomCountdownTimerUI.java
@@ -0,0 +1,13 @@
+package cct.gui;
+
+import javax.swing.JFrame;
+
+public class CustomCountdownTimerUI extends JFrame
+{
+	private static final String TITLE = "Countdown Timer";
+
+	public CustomCountdownTimerUI()
+	{
+		super(TITLE);
+	}
+}
---
+++ CustomCountdownTimer/src/cct/model/CountdownTimer.java
@@ -0,0 +1,10 @@
+package cct.model;
+
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CountdownTimer 
+{
+
+}
