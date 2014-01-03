713cacf0a4d4289d64d65c238d388fc96cd973cd
NicholasColotouros <agentnnc@gmail.com>
2014-01-02 20:53:56 -0500

    started model implementation

--- CustomCountdownTimer/src/cct/model/CountdownTimer.java
+++ CustomCountdownTimer/src/cct/model/CountdownTimer.java
@@ -1,10 +1,26 @@
 package cct.model;
 
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.ArrayList;
 
+//the model that will be saved, loaded and used when making the timer
+//TODO: implement serializability
 public class CountdownTimer 
 {
-
+	TimeInterval duration;
+	ArrayList<TimeInterval> reminders;
+	
+	CountdownTimer(TimeInterval pDuration)
+	{
+		duration = pDuration;
+		reminders = new ArrayList<TimeInterval>();
+		reminders.add(new TimeInterval());
+	}
+	
+	CountdownTimer(TimeInterval pDuration, ArrayList<TimeInterval> pReminders)
+	{
+		duration = pDuration;
+		//TODO: sort reminders starting at 0:0, remove duplicates
+		reminders = pReminders;
+	}
+	
 }
--- CustomCountdownTimer/src/cct/model/TimeInterval.java
+++ CustomCountdownTimer/src/cct/model/TimeInterval.java
@@ -7,6 +7,12 @@ public class TimeInterval
 	private int seconds;
 	private int minutes;
 	
+	TimeInterval()
+	{
+		seconds = 0;
+		minutes = 0;
+	}
+	
 	TimeInterval(int pSeconds, int pMinutes) throws NegativeInputException
 	{
 		if(pSeconds < 0 || pMinutes < 0) throw new NegativeInputException();
