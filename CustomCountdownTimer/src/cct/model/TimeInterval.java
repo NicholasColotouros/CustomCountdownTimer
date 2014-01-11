f04ccc7d226c0a959e02fa70e58a795dabd80582
NicholasColotouros <agentnnc@gmail.com>
2014-01-11 12:43:54 -0500

    added javadoc comments

--- CustomCountdownTimer/src/cct/model/TimeInterval.java
+++ CustomCountdownTimer/src/cct/model/TimeInterval.java
@@ -1,19 +1,33 @@
 package cct.model;
 
 import cct.exceptions.NegativeInputException;
+import cct.exceptions.TimerOverflowException;
 
 public class TimeInterval 
 {
 	private int seconds;
 	private int minutes;
 	
+	/**
+	 * Placeholder constructor for when a timer of 0 length is needed
+	 */
 	TimeInterval()
 	{
 		seconds = 0;
 		minutes = 0;
 	}
 	
-	TimeInterval(int pSeconds, int pMinutes) throws NegativeInputException
+	/**
+	 * Constructs a time interval based on the times given
+	 * 
+	 * @param pSeconds The number of seconds that come after the number of minutes. 
+	 * If more than 59 seconds are input additional minute(s) will be added to the minutes 
+	 * and the seconds value will be changed accordingly to keep the time interval equivalent.
+	 * @param pMinutes The number of minutes.
+	 * @throws NegativeInputException when a negative number is passed as an argument.
+	 * @throws TimerOverflowException when a time interval greater than what is supported is given. 
+	 */
+	TimeInterval(int pSeconds, int pMinutes) throws NegativeInputException, TimerOverflowException
 	{
 		if(pSeconds < 0 || pMinutes < 0) throw new NegativeInputException();
 		
@@ -23,6 +37,8 @@ public class TimeInterval
 			pMinutes = pMinutes + extraMinutes;
 			pSeconds = pSeconds - extraMinutes*60;
 		}
+		
+		if(pSeconds > 99) throw new TimerOverflowException();
 		
 		seconds = pSeconds;
 		minutes = pMinutes;
